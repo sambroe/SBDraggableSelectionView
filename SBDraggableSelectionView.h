22151768fe32d7cc8183930695538dac88a6991d
Sam Broe <sam@expandtheroom.com>
2012-10-16 14:45:51 -0400

    Adding initial files

---
+++ SBDraggableSelectionView.h
@@ -0,0 +1,20 @@
+//
+//  SBDraggableSelectionView.h
+//  Parkr
+//
+//  Created by Sam Broe on 10/14/12.
+//  Copyright (c) 2012 Sam Broe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBDraggableSelectionView : UIView
+
+@property (nonatomic, assign) NSInteger currentXDir;
+@property (nonatomic, assign) NSInteger currentYDir;
+@property (nonatomic, assign) CGPoint currentTouchPoint;
+@property (nonatomic, readonly) UIView *viewForPointLocation;
+
+-(void)evalutateSelectionForPoint:(CGPoint)point directon:(CGPoint)direction;
+
+@end
---
+++ SBDraggableSelectionView.m
@@ -0,0 +1,100 @@
+//
+//  SBDraggableSelectionView.m
+//  Parkr
+//
+//  Created by Sam Broe on 10/14/12.
+//  Copyright (c) 2012 Sam Broe. All rights reserved.
+//
+
+#import "SBDraggableSelectionView.h"
+
+@implementation SBDraggableSelectionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+-(UIView *)viewForPointLocation
+{
+    return self;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self.viewForPointLocation];
+    
+    _currentXDir = 0;
+    _currentYDir = 0;
+    
+    _currentTouchPoint = point;
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self.viewForPointLocation];
+    
+    NSInteger xDir = 0;
+    NSInteger yDir = 0;
+    
+    if (point.x > _currentTouchPoint.x)
+    {
+        xDir = 1;
+    }
+    else if (point.x < _currentTouchPoint.x)
+    {
+        xDir = -1;
+    }
+    
+    if (point.y > _currentTouchPoint.y)
+    {
+        yDir = 1;
+    }
+    else if (point.y < _currentTouchPoint.y)
+    {
+        yDir = -1;
+    }
+    
+    [self evalutateSelectionForPoint:point directon:CGPointMake(xDir, yDir)];
+    
+    _currentXDir = xDir;
+    _currentYDir = yDir;
+    _currentTouchPoint = point;
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self.viewForPointLocation];
+    
+    [self evalutateSelectionForPoint:point directon:CGPointZero];
+    
+    _currentXDir = 0;
+    _currentYDir = 0;
+    _currentTouchPoint = CGPointZero;
+}
+
+-(void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self.viewForPointLocation];
+    
+    [self evalutateSelectionForPoint:point directon:CGPointZero];
+    
+    _currentXDir = 0;
+    _currentYDir = 0;
+    _currentTouchPoint = CGPointZero;
+}
+
+-(void)evalutateSelectionForPoint:(CGPoint)point directon:(CGPoint)direction
+{
+    
+}
+
+@end
